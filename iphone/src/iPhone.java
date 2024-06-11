48b8ccdbb285277ee3f98e5da5c00f120c90e83b
Dalton Witz <daltonfranck00@gmail.com>
2024-06-11 18:28:51 -0300

    first commit

---
+++ iphone/src/iPhone.java
@@ -0,0 +1,49 @@
+public class iPhone implements ReprodutorMusical, AparelhoTelefonico, NavegadorNaInternet {
+    // Implementação dos métodos de ReprodutorMusical
+    @Override
+    public void tocar() {
+        System.out.println("Reproduzindo música...");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Música pausada.");
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+        System.out.println("Música selecionada: " + musica);
+    }
+
+    // Implementação dos métodos de AparelhoTelefonico
+    @Override
+    public void ligar(String numero) {
+        System.out.println("Ligando para: " + numero);
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Atendendo chamada...");
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando correio de voz...");
+    }
+
+    // Implementação dos métodos de NavegadorNaInternet
+    @Override
+    public void exibirPagina(String url) {
+        System.out.println("Exibindo página: " + url);
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Nova aba adicionada.");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Página atualizada.");
+    }
+}
